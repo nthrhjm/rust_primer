106d0bbd81ec986bdc4e3c193ca24172f64f33bc
Hajime Nutahara <hajime.nutahara@gmail.com>
2020-06-10 13:06:36 +0900

    Update main.rs
    浮動小数点(f32, f64)
    文字型(char)
    文字列型(&str, string)

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -15,4 +15,43 @@ fn main() {
     let b = 24;
 
     println!("{} + {} = {}", a, b, add(a, b));
+
+    //Rustの浮動小数点はf32とf64があり、型アノテーションを省略するとf64として扱われる
+    let x = 100.234;
+    println!("x is {}", x);
+    let x: f64 = 100.234;
+    println!("x is {}", x);
+
+    //真偽値の型はbool
+    let boole: bool = true;
+    println!("boole is {}", boole);
+
+    //文字型(char)
+    let c = 'あ';
+    let dog = '🐶';
+    let cat: char = '🐱';
+    println!("{} {} {}", c, dog, cat);
+
+    //文字列(&str)
+    let dog = "DOG";
+    let cat = "CAT";
+    println!("{} and {}", dog, cat);
+
+    //文字列(String)
+    let a = String::from("Hello Rust world.");
+    println!("{}", a);
+
+    //Stringは文字連結(+)ができる
+    let s1 = String::from("Hello");
+    let s2 = String::from("Rust");
+    let s3 = String::from("world.");
+    let s = s1 + " " + &s2 + " " + &s3; //2文字目以降は&をつける（借用）
+    println!("{}", s);
+
+    //format!マクロを使った文字列連結
+    let s4 = String::from("Hello");
+    let s5 = String::from("Rust");
+    let s6 = String::from("world.");
+    let s = format!("{} {} {}", s4, s5, s6);
+    println!("{}", s);
 }
