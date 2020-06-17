bd23673d05f2e3173f043375def380736eff1d22
Hajime Nutahara <hajime.nutahara@gmail.com>
2020-06-18 01:45:03 +0900

    Update rectangles/src/main.rs
    自身(self)ともう一つのRectangle構造体をパラメーターとして取るメソッドcan_hold()
    u32を1個受け取りRectangle構造体を返す関連関数 square()

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -9,6 +9,21 @@ impl Rectangle {
         //メソッドの定義
         self.width * self.height
     }
+    //もう一つのRectangle構造体の不変参照を受け取り
+    //自身のRectanngle構造体の中に収まるかを返す関数
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+
+    //関連関数
+    //implブロック内に定義されるがselfをパラメーターとして取らない関数（メソッドではない）
+    //関連関数は構造体の新規インスタンスを返すコンストラクタによく使用される。
+    fn square(size: u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            height: size,
+        }
+    }
 }
 fn main() {
     let width1 = 30;
@@ -47,6 +62,25 @@ fn main() {
 
     //Rectangle構造体に定義されたarea()methodを使用する
     println!("The area of the rect3 is {} square pixels.", rect3.area());
+
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect3.can_hold(&rect3));
+
+    //関連関数square()を呼び出す
+    //関連関数の呼び出しは`構造体::関連関数`という記法を使う
+    println!("sq is {:?}", Rectangle::square(3)); //sq is Rectangle { width: 3, height: 3 }
 }
 //パラメーターをふたつ取る関数
 fn area(width: u32, height: u32) -> u32 {
