10ca67e1c2a619b6121bf49e550a69802dd0ca61
Hajime Nutahara <hajime.nutahara@gmail.com>
2020-06-18 17:18:54 +0900

    superを使ってtestsモジュールから同階層のclientモジュールを読み込む

--- communicator/src/lib.rs
+++ communicator/src/lib.rs
@@ -3,8 +3,11 @@ pub mod network;
 
 #[cfg(test)]
 mod tests {
+    use super::client; //親階層からuseで取り込む
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+        // assert_eq!(2 + 2, 4);
+        //use super::clientで取り込んでいるので以下のコードが実行できる
+        client::connect();
     }
 }
